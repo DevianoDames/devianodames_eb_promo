7e051b588c22c79e16e5a3d7e64d194148a2873a
DevianoDames <d_dames183654@fanshaweonline.ca>
2023-11-12 07:10:21 -0500

    added xray

--- js/main.js
+++ js/main.js
@@ -28,5 +28,48 @@
       hotspot.addEventListener("mouseout", hideInfo);
     });
   })();
+
+  (() => {
+    let imageCon = document.querySelector('#imageCon'),
+        drag = document.querySelector('.image-drag'),
+        left = document.querySelector('.image-left'),
+        dragging = false,
+        min = 0,
+        max = imageCon.offsetWidth;
+
+        function onDown(){
+            dragging = true;
+            console.log("Set to false")
+        }
+        function onUp() {
+            dragging = true;
+            console.log("Set to false")
+        }
+
+        function onMove(event){
+            //console.log("on move called");
+            if(dragging===true){
+                //console.log("dragging");
+                let x = event.clientX - imageCon.getBoundingClientRect(). left;
+                console.log(x);
+
+                if(x < min){
+                    x = min;
+                  }  else if ( x > max) {
+                        x = max-10;
+                  }
+                    
+                
+
+                drag.style.left = x + "px";
+                left.style.width = x + "px";
+
+            }
+        }
+
+        drag.addEventListener('mousedown',onDown);
+        document.body.addEventListener('mouseup',onUp)
+        document.body.addEventListener('mousemove',onMove);
+})();
   
   
